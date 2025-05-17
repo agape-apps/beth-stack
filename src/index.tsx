85e73b404be066cbfbbc8281699661019544e7b4
liawagner <liawagner@yahoo.com>
2025-05-17 18:19:58 +0800

    feat: refactor shopping list components into separate file

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import { eq } from "drizzle-orm";
 import { html } from "@elysiajs/html";
 import * as elements from "typed-html";
 import { db } from "./db";
-import { Shopping, shoppingItems } from "./db/schema";
+import { shoppingItems } from "./db/schema";
+import { BaseHtml, ShoppingItem, ShoppingList } from "./components/shoppinglist";
 
 // for serving static pages with images
 import { staticPlugin } from "@elysiajs/static";
@@ -130,84 +131,5 @@ console.log(
   `Your application is running at http://${app.server?.hostname}:${app.server?.port}`
 );
 
-const BaseHtml = ({ children }: any) => `
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>The BETH Stack</title>
-    <script src="https://unpkg.com/htmx.org@1.9.3"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
-</head>
-
-${children}
-`;
-
-function ShoppingItem({ id, name, purchased }: Shopping) {
-  return (
-    <div class="box mb-2 is-flex is-align-items-center is-justify-content-space-between">
-      <div class="is-flex is-align-items-center">
-        <input
-          type="checkbox"
-          class="mr-4"
-          checked={purchased}
-          hx-post={`/shopping/${id}/toggle`}
-          hx-target="closest div.box"
-          hx-swap="outerHTML"
-        />
-        <p class="is-size-5">{name}</p>
-      </div>
-      <button
-        class="button is-small is-danger"
-        hx-delete={`/shopping/${id}`}
-        hx-target="closest div.box"
-        hx-swap="outerHTML"
-      >
-        X
-      </button>
-    </div>
-  );
-}
-
-function ShoppingList({ shoppingItems }: { shoppingItems: Shopping[] }) {
-  return (
-    <div class="container">
-      <div class="section">
-        <h1 class="title has-text-centered has-text-info is-2">Shopping List</h1>
-        <div class="columns is-centered">
-          <div class="column is-half">
-            <div class="card">
-              <div class="card-content">
-                {shoppingItems.map((shoppingItem: Shopping) => (
-                  <ShoppingItem {...shoppingItem} />
-                ))}
-                <ShoppingForm />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
-
-function ShoppingForm() {
-  return (
-    <form
-      class="field has-addons mt-5"
-      hx-post="/shopping"
-      hx-swap="beforebegin"
-      _="target"
-    >
-      <div class="control is-expanded">
-        <input type="text" name="name" class="input is-info" placeholder="Add item" />
-      </div>
-      <div class="control">
-        <button type="submit" class="button is-info">Add</button>
-      </div>
-    </form>
-  );
-}
 
